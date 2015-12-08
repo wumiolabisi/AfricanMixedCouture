076c3d3465d98bae55a578ae8ce24c35d72366ea
wumiolabisi <wumi.olabisi@gmail.com>
2015-12-08 15:58:02 +0100

    Récupération en base des images + modals
    Div tops

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ if((isset($_POST['email'])) && (isset($_POST['motdepasse'])))
                 {
                     ?><script>alert("ERROR : Mauvais identifiant ou mot de passe.  Veuillez réessayer");</script><?php
                 }
+				
+				mysqli_close();
             }    
 
 ?>
@@ -132,11 +134,9 @@ A propos de Corinne Olabisi
 Corinne Olabisi est   
 </div>
 
-<div id="tops" align="center">
-<h2 align="center">Les tops</h2>
-<button type="button" class="btn btn-lg" data-toggle="modal" data-target="#top1" onclick="afficherModal(1);">
-<img class="images" src="img/tops/top1-1.jpg"/>
-</button>
+<div id="tops" class="modalalignement">
+<h2 align="center" class="titre2">Les tops</h2>
+
 <?php
 
 $dsn ="mysql:dbname=africanmixedcouture;host=localhost";
@@ -144,20 +144,20 @@ $username="root";
 $passwd="";   
 
 $pdo = new PDO($dsn, $username, $passwd);
-$resultatCreation = $pdo ->query("SELECT creation.libelle,creation.id_creation  FROM creation INNER JOIN categorie ON categorie.id_categorie = creation.idcategorie WHERE categorie.libelle = 'tops'");
+$resultatCreation = $pdo ->query("SELECT creation.libelle,creation.id_creation,creation.description  FROM creation INNER JOIN categorie ON categorie.id_categorie = creation.idcategorie WHERE categorie.libelle = 'tops'");
 $listeCreation = $resultatCreation->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($listeCreation as $value)
 {
         $libelleCreation = $value['libelle'];
         $idCreation = $value['id_creation'];
+		$descriptionCreation = $value['description'];
 
  
-         ?> Crea libelle <?php echo $libelleCreation  ?>
-          
-       <?php
-	   
-$resultatPhoto = $pdo ->query("SELECT photo.chemin FROM photo INNER JOIN creation ON creation.id_creation = photo.idcreation WHERE creation.id_creation = $idCreation ");
+         ?> <h4> <?php echo $libelleCreation  ?></h4> <?php
+
+
+$resultatPhoto = $pdo ->query("SELECT photo.chemin FROM photo INNER JOIN creation ON creation.id_creation = photo.idcreation AND photo.chemin LIKE '%1.jpg' WHERE creation.id_creation = $idCreation");
 $listePhoto = $resultatPhoto->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($listePhoto as $value2)
@@ -165,17 +165,55 @@ foreach($listePhoto as $value2)
         $cheminPhoto = $value2['chemin'];
 
  
-         ?><img class="images" src="<?php echo $cheminPhoto ?>"/>
-          
+         ?><!-- Declenchement du modal -->
+<button type="button" class="btn btn-lg" data-toggle="modal" data-target="#myModal<?php echo $idCreation ?>"><img class="imagesmodals" src="<?php echo $cheminPhoto ?>"/></button>
+<!-- Modal -->
+<div id="myModal<?php echo $idCreation ?>" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title"><?php echo $libelleCreation ?></h4>
+      </div>
+      <div class="modal-body">
        <?php
 	   
 	   
+$resultatPhoto2 = $pdo ->query("SELECT photo.chemin FROM photo INNER JOIN creation ON creation.id_creation = photo.idcreation WHERE creation.id_creation = $idCreation");
+$listePhoto2 = $resultatPhoto2->fetchAll(PDO::FETCH_ASSOC);
 	   
+	  
+
+foreach($listePhoto2 as $value3)
+{
+        $cheminPhoto2 = $value3['chemin'];
+
+?>
+
+<img class="images" src="<?php echo $cheminPhoto2?>"/>
+<?php  }?>  	  <h4><?php echo $descriptionCreation ?></h4>
+ 
+      </div>
+
+      <div class="modal-footer"">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+         <button type="button" class="btn btn-default" data-dismiss="modal">Favoris</button>
+
+	 </div>
+    </div>
+
+  </div>
+</div>
+          
+       <?php
+	     
 	   
    }  
 	}
-	  
-
+	
+	 
 
 ?>
 </div>
